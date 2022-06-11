72ba4420c590848f1d7c9686910d718e6a1cfd89
raven <zlat.dominic.francisc@gmail.com>
2022-06-11 20:12:02 +0300

    No Relay Stations / Payloads place holder for the dock status

--- src/components/PayloadList.vue
+++ src/components/PayloadList.vue
@@ -14,10 +14,23 @@ export default {
 
 <template>
     <div id="payload_list_container_actual">
+        <Label v-if="payload_array == undefined || payload_array.length == 0" id="no_payloads_l" color="#515151" v-text="'No Payloads detected in storage'"></Label>
         <PayloadListItem v-for="(payload_obj, index) in payload_array" :key="index" :payload_obj="payload_obj"></PayloadListItem>
     </div>
 </template>
 <style scoped>
+#no_payloads_l{
+    top: 40%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 93.498452012%;
+    display: flex;
+    align-items: center;
+    border: solid 1px #515151;
+    justify-content: center;
+    height: 26.704545455%;
+    font-size: 1vw;
+}
 #payload_list_container_actual{
     position: absolute;
     top: 21.524663677%;
--- src/components/RelayStationList.vue
+++ src/components/RelayStationList.vue
@@ -1,39 +1,69 @@
 
 <script setup>
 import RelayStationListItem from "@/components/RelayStationListItem.vue";
-    
+import Label from "@/components/Label.vue";
 </script>
 
 <script>
 export default {
-    props: {
-        relay_station_array: [],
-        selected_dock_obj: ""
-    },
-    methods:{
-        parse_relay_station_array(){
-            let relay_station_array_actual = [];
-            for(let ix = 0; ix < this.relay_station_array.length; ix++){
-                if(this.relay_station_array[ix].dock_array.find(({dock_id}) => dock_id == this.selected_dock_obj.dock_id) != null){
-                    relay_station_array_actual.push(this.relay_station_array[ix]);
-                }
-            }
-            return relay_station_array_actual;
+  props: {
+    relay_station_array: [],
+    selected_dock_obj: "",
+  },
+  methods: {
+    parse_relay_station_array() {
+      let relay_station_array_actual = [];
+      for (let ix = 0; ix < this.relay_station_array.length; ix++) {
+        if (
+          this.relay_station_array[ix].dock_array.find(
+            ({ dock_id }) => dock_id == this.selected_dock_obj.dock_id
+          ) != null
+        ) {
+          relay_station_array_actual.push(this.relay_station_array[ix]);
         }
-    }
-}
+      }
+      return relay_station_array_actual;
+    },
+  },
+};
 </script>
 <template>
   <div class="relay_stations_list_container">
-      <RelayStationListItem v-for="(relay_station, index) in parse_relay_station_array()" :key="index" :relay_station_obj="relay_station" color="#00FFF0"></RelayStationListItem>
+    <Label
+      v-if="parse_relay_station_array().length == 0"
+      id="no_relay_stations_l"
+      color="#515151"
+      v-text="`No Relay Stations connected to this dock`"
+    ></Label>
+    <RelayStationListItem
+      v-for="(relay_station, index) in parse_relay_station_array()"
+      :key="index"
+      :relay_station_obj="relay_station"
+      color="#00FFF0"
+    ></RelayStationListItem>
   </div>
 </template>
 <style scoped>
+#no_relay_stations_l {
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: calc(93.498452012% - 10%);
+  display: flex;
+  align-items: center;
+  border: solid 1px #515151;
+  padding-right: 5%;
+  padding-left: 5%;
+  justify-content: center;
+  height: 37.704545455%;
+  font-size: 1vw;
+  text-align: center;
+}
 .relay_stations_list_container {
-    position: absolute;
-    top: 18.061674009%;
-    left: 0;
-    width: 99.8%;
-    height: 81.938325991%;
+  position: absolute;
+  top: 18.061674009%;
+  left: 0;
+  width: 99.8%;
+  height: 81.938325991%;
 }
 </style>
