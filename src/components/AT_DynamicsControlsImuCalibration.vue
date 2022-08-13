9a1d52fa0f1af21b1b5fd466ed7b3e434a8ec368
raven <zlat.dominic.francisc@gmail.com>
2022-08-13 19:21:37 +0300

    State Save

--- src/components/AT_DynamicsControlsImuCalibration.vue
+++ src/components/AT_DynamicsControlsImuCalibration.vue
@@ -3,56 +3,130 @@
 import BaseLabel from "@/components/BaseLabel.vue";
 import VerticalLine from "@/components/VerticalLine.vue";
 import HorizontalLine from "@/components/HorizontalLine.vue";
+import DynamicsControlsImuCalibrationStageCard from "@/components/AT_DynamicsControlsImuCalibrationStageCard.vue";
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      isRunning: false,
+    };
+  },
+  methods: {
+    ImuCalibrationButtonOnClick() {
+      this.isRunning = true;
+    },
+  },
+};
 </script>
 
 <template>
   <div class="dynamics_imu_calibration_controller_container">
     <BaseLabel v-text="'IMU Calibration'" class="x_controls_l"></BaseLabel>
-    <div id="dynamics_imu_calibration_initialization_container">
+    <div
+      class="fade"
+      v-if="!isRunning"
+      id="dynamics_imu_calibration_initialization_container"
+    >
       <BaseLabel
         id="imu_calibration_ini_l"
         v-text="
           'Place the Vulture on a completely flat surface then click the ‘Start Calibration’ button.'
         "
       ></BaseLabel>
-      <div id="imu_calibration_ini_btn">
-        <BaseLabel id="imu_calibration_ini_btn_l" v-text="'Start Calibration'"></BaseLabel>
+      <div @click="ImuCalibrationButtonOnClick" id="imu_calibration_ini_btn">
+        <BaseLabel
+          id="imu_calibration_ini_btn_l"
+          v-text="'Start Calibration'"
+        ></BaseLabel>
       </div>
-      <BaseLabel color="#5B5B5B" id="ini_calibration_l_2" v-text="'The calibration is done for both IMUs at the same time'"></BaseLabel>
+      <BaseLabel
+        color="#5B5B5B"
+        id="ini_calibration_l_2"
+        v-text="'The calibration is done for both IMUs at the same time'"
+      ></BaseLabel>
     </div>
-    <div id="dynamics_imu_calibration_pending_container"></div>
+    <div
+      class="fade"
+      v-if="isRunning"
+      id="dynamics_imu_calibration_pending_container"
+    >
+      <BaseLabel
+        id="imu_calibration_pending_l"
+        v-text="'In Progress'"
+      ></BaseLabel>
+      <DynamicsControlsImuCalibrationStageCard
+      id="calibration_pending_stage_card_0"
+        color="#FFF"
+        label="Validating Stability"
+        :isActive="true"
+        :completionStatus="20"
+      ></DynamicsControlsImuCalibrationStageCard>
+    </div>
   </div>
 </template>
 <style scoped>
-#ini_calibration_l_2{
-    top: 75.281407%;
-    left: 0%;
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    font-size: .95vw;
-    text-align: center;
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
 }
-#imu_calibration_ini_btn_l{
-    font-size: 1vw;
+.fade {
+  animation: fade linear 0.1s;
 }
-#imu_calibration_ini_btn{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 62.568306011%;
-    height: 15.075376884%;
-    border: solid 1px #0500FF;
-    transform: translate(-50%);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background-color: #0500FF00;
-    transition: all linear 0.1s;
+#calibration_pending_stage_card_0{
+  left: 50%;
+  transform: translate(-50%);
+  top: 31.909547739%;
 }
-#imu_calibration_ini_btn:hover{
-    background-color: #0500FF20;
+#imu_calibration_pending_l {
+  top: 15.027523%;
+  width: 100%;
+  height: 25.879396985%;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  padding-top: 3%;
+  border-top: solid 1px #0500ff;
+  background: radial-gradient(
+    52.7% 52.7% at 50% 0%,
+    rgba(5, 0, 255, 0.4) 0%,
+    rgba(5, 0, 255, 0) 105%
+  );
+}
+#ini_calibration_l_2 {
+  top: 75.281407%;
+  left: 0%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 0.95vw;
+  text-align: center;
+}
+#imu_calibration_ini_btn_l {
+  font-size: 1vw;
+}
+#imu_calibration_ini_btn {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 62.568306011%;
+  height: 15.075376884%;
+  border: solid 1px #0500ff;
+  transform: translate(-50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #0500ff00;
+  transition: all linear 0.1s;
+}
+#imu_calibration_ini_btn:hover {
+  background-color: #0500ff20;
 }
 #imu_calibration_ini_l {
   top: 12.060301508%;
---
+++ src/components/AT_DynamicsControlsImuCalibrationStageCard.vue
@@ -0,0 +1,64 @@
+
+<script setup lang="ts">
+import BaseLabel from "@/components/BaseLabel.vue";
+import HorizontalLine from "@/components/HorizontalLine.vue";
+</script>
+<script lang="ts">
+export default {
+  props: {
+    color: { default: "#FFF" },
+    backgroundColor: { default: "#0500FF20" },
+    label: { default: "--" },
+    isActive: { default: false },
+    completionStatus: { default: 0 },
+  },
+};
+</script>
+
+<template>
+  <div
+    :style="`background-color: ${backgroundColor};`"
+    class="dynamics_controls_imu_calibration_stage_card_container"
+  >
+    <BaseLabel
+      class="calibration_stage_card_l"
+      :color="color"
+      v-text="label"
+    ></BaseLabel>
+    <div v-if="isActive" class="loading_bar_container">
+      <HorizontalLine
+        :style="`left: ${completionStatus - 100}%`"
+        class="loading_bar_slider"
+        color="#2400FF"
+      ></HorizontalLine>
+    </div>
+  </div>
+</template>
+<style scoped>
+.calibration_stage_card_l {
+  font-size: 1vw;
+}
+.loading_bar_slider {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+.loading_bar_container {
+  position: absolute;
+  top: 85.714285714%;
+  left: 50%;
+  transform: translate(-50%);
+  width: 73.770491803%;
+  height: 0.01vw;
+  background-color: #0c0055;
+  overflow: hidden;
+}
+.dynamics_controls_imu_calibration_stage_card_container {
+  position: absolute;
+  height: 15.829145729%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
