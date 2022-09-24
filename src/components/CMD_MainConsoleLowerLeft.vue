d4c59751b4b0e6539cf82c53cfd45a3c038e4d93
raven <zlat.dominic.francisc@gmail.com>
2022-09-24 14:11:23 +0300

    Minimap UX testing

--- src/components/CMD_MainConsoleLowerLeft.vue
+++ src/components/CMD_MainConsoleLowerLeft.vue
@@ -12,26 +12,58 @@ import MainConsoleLowerLeft from "@/components/CMD_MainConsoleLowerLeft.vue";
 import isMobile from "@/composables/isMobile.ts";
 import percentage from "@/composables/percentage.ts";
 import scale from "@/composables/scale.ts";
+
+// import "leaflet/dist/leaflet.css";
+// import L from "mapbox-gl-leaflet";
+// import mapboxGL from "mapbox-gl";
 </script>
 
 
 <script lang="ts">
+let root = document.documentElement;
 export default {
   methods: {
-    UI_assessor() {
-      let root = document.documentElement;
+    mapSetup() {
+      var map = L.map('minimap_container').setView([30.42110, -93.78787], 5);
+      var gl = L.mapboxGL({
+        attribution: "\u003ca href=\"https://www.maptiler.com/copyright/\" target=\"_blank\"\u003e\u0026copy; MapTiler\u003c/a\u003e \u003ca href=\"https://www.openstreetmap.org/copyright\" target=\"_blank\"\u003e\u0026copy; OpenStreetMap contributors\u003c/a\u003e",
+        style: 'https://api.maptiler.com/maps/96f0d0fa-e9c9-43b1-a8ff-09fd5b45a351/style.json?key=R1cyh6lj1mTfNEycg2N1'
+      }).addTo(map);
+    },
+    primaryContainerStyleController() {
       let primaryContainerWidth = percentage(
         scale(450, 1920),
         document.documentElement.clientWidth
       );
-      this.screenAspectRatio = root.clientWidth / root.clientHeight;
       this.primaryContainerStyle = `width: ${
         primaryContainerWidth +
         Math.abs(1.971252567 - this.screenAspectRatio) * 10
       }%;`;
     },
+    secondary0ContainerStyleController() {
+      let screenAspectRatioOffset =
+        Math.abs(1.971252567 - this.screenAspectRatio) * 10;
+      let secondary0ContainerWidth = percentage(
+        scale(124, 1920),
+        document.documentElement.clientWidth
+      );
+      let secondary0ContainerLeft = percentage(
+        scale(531, 1920),
+        document.documentElement.clientWidth
+      );
+      this.secondary0ContainerStyle = `width: ${
+        secondary0ContainerWidth + screenAspectRatioOffset - 1
+      }%; left: ${secondary0ContainerLeft + screenAspectRatioOffset};`;
+    },
+    UI_assessor() {
+      /*handles scaling of the ui relative to the svg background*/
+      this.screenAspectRatio = root.clientWidth / root.clientHeight;
+      this.primaryContainerStyleController();
+      this.secondary0ContainerStyleController();
+    },
   },
   mounted() {
+    this.mapSetup();
     this.UI_assessor();
     window.addEventListener("resize", () => {
       this.UI_assessor();
@@ -58,7 +90,9 @@ export default {
     <div
       id="cmd_main_console_lower_left_primary_container"
       :style="primaryContainerStyle"
-    ></div>
+    >
+      <div id="minimap_container"></div>
+    </div>
     <div
       id="cmd_main_console_lower_left_secondary_0_container"
       :style="secondary0ContainerStyle"
@@ -81,5 +115,12 @@ export default {
   width: 6.458333333%;
   height: 4.074074074%;
   background-color: #55555540;
+}
+#minimap_container {
+  position: absolute;
+  top: 0;
+  width: 30%;
+  height: 100%;
+  background-color: #0500ff40;
 }
 </style>
